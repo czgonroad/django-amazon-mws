dc8b920c53ff44ceca7b845b8112cd1ea3bb0029
James Hiew <james@hiew.net>
2018-05-29 11:33:41 +0100

    Add an MWSCredentials model

--- djmws/models.py
+++ djmws/models.py
@@ -1,4 +1,7 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+
+User = get_user_model()
 
 
 class RegionManager(models.Manager):
@@ -31,3 +34,37 @@ class Marketplace(models.Model):
 
     def __str__(self):
         return self.code
+
+
+class MWSCredentials(models.Model):
+    user = models.ForeignKey(User, models.CASCADE,
+                             related_name='mws_credentials')
+    region = models.ForeignKey(Region, models.PROTECT)
+    seller_id = models.CharField(max_length=32, verbose_name='Seller ID')
+    access_key = models.CharField(max_length=32)
+    secret_key = models.CharField(max_length=64)
+
+    class Meta(object):
+        verbose_name = 'MWS Credentials'
+        verbose_name_plural = 'MWS Credentials'
+        unique_together = (
+            ('user', 'region', )
+        )
+
+    def _as_dictionary(self):
+        return {
+            'access_key': self.access_key,
+            'secret_key': self.secret_key,
+            'account_id': self.seller_id
+        }
+
+    def get_api(self, API, marketplace_code):
+        # assert issubclass(API, MWS) and type(marketplace) is Marketplace
+        return API(
+            region=marketplace_code,
+            domain=self.region.endpoint,
+            **self._as_dictionary(),
+        )
+
+    def __str__(self):
+        return 'Credentials for {} ({})'.format(self.user, self.region)
